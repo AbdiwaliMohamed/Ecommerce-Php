cd1be828f641afce36644644f474b6bbbd01b330
Abdiwali <46087190+abdiwali145@users.noreply.github.com>
2020-05-11 10:45:22 +0200

    intial commit

---
+++ QualityBagsEcommerce/data_layer/credential.inc.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'root'; //Your dochyper username
+$pass = 'Abdiwali12345'; //Your dochyper password
+$name = 'qualitybagsdatabase'; //The database name you want to use
+?>
